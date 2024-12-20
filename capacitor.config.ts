93603846367ee96043c2f07fff1a2201ea1878b1
ailu-torreval <ailu.torreval@gmail.com>
2024-12-20 10:06:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mit_id-sandbox',
+  webDir: 'www'
+};
+
+export default config;
