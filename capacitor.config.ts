2883f090fac67f8d2bc9e77c16ea08c0be56fe86
ailu-torreval <ailu.torreval@gmail.com>
2025-01-20 11:59:53 +0100

    testing universal links

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'dk.ionic.mitIdTester',
   appName: 'mit_id-sandbox',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    hostname: 'mitid-test-99d1b.web.app',
+    iosScheme: 'https'
+  },
+  ios: {
+    scheme: 'mit_id-sandbox'
+  },
+  plugins: {
+    CapacitorURLScheme: {
+      schemes: ['mit_id-sandbox']
+    }
+  }
 };
 
 export default config;
-
-
-
